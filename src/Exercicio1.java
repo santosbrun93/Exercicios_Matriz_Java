87a23f7491b24959e33dab18bb1be607665cc3dd
Bruno Silva <santos.brun93@gmail.com>
2022-02-03 21:51:46 -0300

    Exercicio 1 concluído

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
 public class Exercicio1 {
 
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int[][] matriz = new int[4][4];
+
+        int contador = 0;
+
+        for(int i = 0; i < 4 ; i++){
+            for(int j = 0; j < 4 ; j++){
+                matriz[i][j] = random.nextInt(15);
+            }
+        }
+
+        for(int i = 0; i < 4 ; i++){
+            for(int j = 0; j < 4 ; j++){
+                if(matriz[i][j] >= 10){
+                    contador++;
+                }
+            }
+        }
+
+        System.out.println("A matriz possui: "+contador);
     }
 }
