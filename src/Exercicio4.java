faaf76e78b095a478447f27eab4ea054b8c9dcc2
Bruno Silva <santos.brun93@gmail.com>
2022-02-06 11:16:20 -0300

    Exercicio 4 ajustado

--- src/Exercicio4.java
+++ src/Exercicio4.java
@@ -5,6 +5,8 @@ public class Exercicio4 {
     public static void main(String[] args) {
         int[][] matriz = new int[4][4];
 
+        int linhaMaior = 0, colunaMaior = 0;
+
         Random random = new Random();
 
         int maiorValor = 0;
@@ -16,12 +18,18 @@ public class Exercicio4 {
 
                 if(linha == 0 & coluna == 0){
                     maiorValor = matriz[linha][coluna];
+                    linhaMaior = 0;
+                    colunaMaior = 0;
                 }
                 else if (maiorValor < matriz[linha][coluna]){
                     maiorValor = matriz[linha][coluna];
+                    linhaMaior = linha;
+                    colunaMaior = coluna;
                 }
             }
         }
         System.out.println("O Maior valor da matriz é "+maiorValor);
+        System.out.println("A Linha do maior valor da matriz é "+linhaMaior);
+        System.out.println("A Coluna do maior valor da matriz é "+colunaMaior);
     }
 }
