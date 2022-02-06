81c52d57a189202330e042ce5927815552b1f066
Bruno Silva <santos.brun93@gmail.com>
2022-02-06 10:51:45 -0300

    Exercicio 3 concluido

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
 public class Exercicio3 {
 
     public static void main(String[] args) {
+
+        int[][] matriz = new int[4][4];
+
+        //Random random = new Random();
+
+        for(int linha = 0 ; linha < 4; linha++) {
+
+            for(int coluna = 0 ; coluna < 4; coluna++) {
+                matriz[linha][coluna] = linha * coluna;
+            }
+        }
+
+        for(int linha = 0 ; linha < 4; linha++) {
+
+            for(int coluna = 0 ; coluna < 4; coluna++) {
+                System.out.println(matriz[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
+
 
     }
 }
