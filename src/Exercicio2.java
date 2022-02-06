44a9bf8560c12bf21f7d4e3e140dbbdc40b139ed
Bruno Silva <santos.brun93@gmail.com>
2022-02-06 10:41:41 -0300

    Exercicio 2 concluido

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Exercicio2 {
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        int[][] matriz = new int[5][5];
+
+
+
+        for(int linha = 0 ; linha < 5; linha++) {
+
+            for(int coluna = 0 ; coluna < 5; coluna++) {
+                if(linha == coluna) matriz[linha][coluna] = 1 ; //Diagonal principal
+                else matriz[linha][coluna] = 0;
+            }
+        }
+
+        for(int linha = 0 ; linha < 5; linha++) {
+            for(int coluna = 0 ; coluna < 5; coluna++)
+            {
+                System.out.println(matriz[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
+
+    }
 }
--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Exercicio3 {
+public class Exercicio3 {
+
+    public static void main(String[] args) {
+
+    }
 }
