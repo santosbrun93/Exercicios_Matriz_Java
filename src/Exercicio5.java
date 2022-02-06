d333e2a4210cf9c707ae956bd14a90b143ab1b74
Bruno Silva <santos.brun93@gmail.com>
2022-02-06 12:12:33 -0300

    Exercicio 6 concluido

--- src/Exercicio5.java
+++ src/Exercicio5.java
@@ -1,6 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercicio5 {
 
     public static void main(String[] args) {
+        int[][] matriz = new int[5][5];
 
+        int x = 0, colunaMaior = 0;
+
+        Random random = new Random();
+
+        Scanner entrada = new Scanner(System.in);
+
+        for (int linha = 0; linha < 5; linha++) {
+
+            for (int coluna = 0; coluna < 5; coluna++) {
+                matriz[linha][coluna] = random.nextInt(20);
+            }
+        }
+
+        for(int linha = 0 ; linha < 5; linha++) {
+
+            for(int coluna = 0 ; coluna < 5; coluna++) {
+                System.out.println(matriz[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
+
+        System.out.println("Matriz Preenchida!!");
+        System.out.println("Informe um número que deseja encontrar: ");
+        x = entrada.nextInt();
+
+        for (int linha = 0; linha < 5; linha++) {
+
+            for (int coluna = 0; coluna < 5; coluna++) {
+                if(x == matriz[linha][coluna]){
+                    System.out.println("Valor encontrado!!");
+                    System.out.println("Linha: "+linha);
+                    System.out.println("Coluna: "+coluna);
+                }else
+                    System.out.println("\n");
+                    System.out.println("Valor não encontrado nessa linha!!");
+            }
+        }
     }
 }
--- src/Exercicio6.java
+++ src/Exercicio6.java
@@ -1,6 +1,61 @@
+import java.util.Random;
+
 public class Exercicio6 {
 
     public static void main(String[] args) {
+        int[][] matriz1 = new int[4][4];
 
+        int[][] matriz2 = new int[4][4];
+
+        int[][] matriz3 = new int[4][4];
+
+        Random random = new Random();
+
+        for (int linha = 0; linha < 4; linha++) {
+
+            for (int coluna = 0; coluna < 4; coluna++) {
+                matriz1[linha][coluna] = random.nextInt(20);
+                matriz2[linha][coluna] = random.nextInt(20);
+            }
+        }
+
+
+        for (int linha = 0; linha < 4; linha++) {
+
+            for (int coluna = 0; coluna < 4; coluna++) {
+                if(matriz1[linha][coluna] == matriz2[linha][coluna]) {
+                    matriz3[linha][coluna] = matriz1[linha][coluna];
+                }
+                else if(matriz1[linha][coluna] < matriz2[linha][coluna]){
+                    matriz3[linha][coluna] = matriz2[linha][coluna];
+                }else if(matriz2[linha][coluna] < matriz1[linha][coluna]){
+                    matriz3[linha][coluna] = matriz1[linha][coluna];
+                }
+            }
+        }
+
+        for(int linha = 0 ; linha < 4; linha++) {
+
+            for(int coluna = 0 ; coluna < 4; coluna++) {
+                System.out.println(matriz1[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
+
+        for(int linha = 0 ; linha < 4; linha++) {
+
+            for(int coluna = 0 ; coluna < 4; coluna++) {
+                System.out.println(matriz2[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
+
+        for(int linha = 0 ; linha < 4; linha++) {
+
+            for(int coluna = 0 ; coluna < 4; coluna++) {
+                System.out.println(matriz3[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
     }
 }
