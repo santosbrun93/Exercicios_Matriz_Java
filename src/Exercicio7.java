08ab19b877a7c8fba9dab8f03c7b2cf11e3ddf51
Bruno Silva <santos.brun93@gmail.com>
2022-02-08 20:18:38 -0300

    Exercicio 7 concluído

--- src/Exercicio7.java
+++ src/Exercicio7.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class Exercicio7 {
 
     public static void main(String[] args) {
 
+        double[][] matriz = new double[10][10];
+
+        Random random = new Random();
+
+        for (int linha = 0; linha < 10; linha++) {
+            for (int coluna = 0; coluna < 10; coluna++) {
+
+                if (linha < coluna) {
+                    matriz[linha][coluna] = (2 * linha) + (7 * coluna) - 2;
+                } else if (linha > coluna) {
+                    matriz[linha][coluna] = (Math.pow(linha , 3) * 3) - (5 * Math.pow(coluna, 2)) + 1;
+                }
+                else {
+                    matriz[linha][coluna] = Math.pow(linha , 2) * 3 - 1;
+                }
+            }
+        }
+
+        for(int linha = 0 ; linha < 10; linha++) {
+
+            for(int coluna = 0 ; coluna < 10; coluna++) {
+                System.out.printf("%.2f ",matriz[linha][coluna]);
+            }
+            System.out.println("\n");
+        }
     }
 }
