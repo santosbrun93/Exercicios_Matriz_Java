8c925fcc0b1f33ec71088afbb2792f2d3d9c0d20
Bruno Silva <santos.brun93@gmail.com>
2022-02-10 18:31:25 -0300

    Exercicio 23 concluido

--- src/Exercicio23.java
+++ src/Exercicio23.java
@@ -1,6 +1,33 @@
+import java.util.Random;
+
 public class Exercicio23 {
 
     public static void main(String[] args) {
+        Random random = new Random();
 
+        int[][] matrizA = new int[3][3];
+        int[][] matrizB = new int[3][3];
+        int[][] matrizC = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrizA[i][j] = random.nextInt(50);
+                matrizB[i][j] = matrizA[i][j] * matrizA[i][j];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + matrizA[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + matrizB[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
+
