01adae2463d5a65b3a9506e0f11167f1243ac7b5
Bruno Silva <santos.brun93@gmail.com>
2022-02-10 18:13:34 -0300

    Exercicio 22 concluido

--- src/Exercicio17.java
+++ src/Exercicio17.java
@@ -1,6 +1,29 @@
+import java.util.Random;
+
 public class Exercicio17 {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        double[][] matrizProvas_Alunos = new double[10][3];
+
+        double menorNota = 0;
+        int prova1 = 0, prova2 = 0, prova3 = 0;
+
+
+        for(int aluno = 0 ; aluno < 10 ; aluno++){
+            for(int prova = 0 ; prova < 3; prova++){
+                matrizProvas_Alunos[aluno][prova] = random.nextFloat();
+
+                if(prova == 0){
+                        menorNota = matrizProvas_Alunos[aluno][prova];
+                        prova1++;
+
+                }else if(menorNota > matrizProvas_Alunos[aluno][prova] && prova > 0){
+                        menorNota = matrizProvas_Alunos[aluno][prova];
+                }
+            }
+        }
     }
 }
--- src/Exercicio22.java
+++ src/Exercicio22.java
@@ -1,6 +1,41 @@
+import java.util.Random;
+
 public class Exercicio22 {
 
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        int[][] matrizA = new int[3][3];
+        int[][] matrizB = new int[3][3];
+        int[][] matrizC = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrizA[i][j] = random.nextInt(50);
+                matrizB[i][j] = random.nextInt(50);
+                matrizC[i][j] = matrizA[i][j] * matrizB[i][j];
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + matrizA[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("| " + matrizB[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("|  " + matrizC[i][j]);
+            }
+            System.out.println();
+        }
     }
+
 }
